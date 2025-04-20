fa6f6c4aca3a0ee0e472187f0e114109989ab650
dahmanp <dahmanp@my.erau.edu>
2025-04-20 13:38:50 -0700

    Add project

---
+++ ML Agents Production/Assets/CarDriverAI/Scripts/CarAITargetMouse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAITargetMouse : MonoBehaviour {
+
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Vector2 mapMinBounds = new Vector2(-50f, -50f);
+    [SerializeField] private Vector2 mapMaxBounds = new Vector2(50f, 50f);
+
+    private void Start()
+    {
+        StartCoroutine(MoveTargetRoutine());
+    }
+
+    private IEnumerator MoveTargetRoutine()
+    {
+        while (true)
+        {
+            MoveTargetToRandomPosition();
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    private void MoveTargetToRandomPosition()
+    {
+        float randomX = Random.Range(mapMinBounds.x, mapMaxBounds.x);
+        float randomZ = Random.Range(mapMinBounds.y, mapMaxBounds.y);
+        targetTransform.position = new Vector3(randomX, targetTransform.position.y, randomZ);
+    }
+}
